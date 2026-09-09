6a5111da3a18863ac4641a20fa1926efe19ccad1
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-09-09 18:35:03 -0500

    Implementa Ejercicio 05: Clase Mascota con atributos public y metodo mostrarMascota

---
+++ src/ParteA_Reconocimiento/Ejercicio_05/Mascota.java
@@ -0,0 +1,13 @@
+package ParteA_Reconocimiento.Ejercicio_05;
+
+public class Mascota {
+    public String nombre;
+    public String especie;
+    public int edad;
+
+    public void mostrarMascota() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad + " años");
+    }
+}

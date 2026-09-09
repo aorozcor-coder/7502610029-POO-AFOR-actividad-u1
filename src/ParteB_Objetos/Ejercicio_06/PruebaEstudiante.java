0fbcc08da0f4b8ccabccfd547cb3d2a6f0a0b9e3
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-09-09 18:44:40 -0500

    Refactor: Renombra paquete ParteA_Reconocimiento a ParteA_Clases

--- src/ParteB_Objetos/Ejercicio_06/PruebaEstudiante.java
+++ src/ParteB_Objetos/Ejercicio_06/PruebaEstudiante.java
@@ -1,4 +1,19 @@
 package ParteB_Objetos.Ejercicio_06;
 
+import ParteA_Reconocimiento.Ejercicio_01.Estudiante;
+
 public class PruebaEstudiante {
+    public static void main(String[] args) {
+
+        Estudiante est = new Estudiante();
+
+
+        est.nombre = "Carlos Andrés Gómez";
+        est.codigo = "20242005";
+        est.semestre = 2;
+
+
+        System.out.println("--- Información del Estudiante ---");
+        est.mostrarInfo();
+    }
 }

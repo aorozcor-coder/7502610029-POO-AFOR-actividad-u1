46cac0fa2b047fb9ee0acf83bf102b487c89cccc
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-09-10 11:37:46 -0500

    Ejercicio 32: Instancia dos productos en la clase pruebaproducto

--- src/ParteF_VariasFormasConstruir/Ejercicio_32/PruebaProducto.java
+++ src/ParteF_VariasFormasConstruir/Ejercicio_32/PruebaProducto.java
@@ -1,4 +1,13 @@
 package ParteF_VariasFormasConstruir.Ejercicio_32;
 
+import ParteF_VariasFormasConstruir.Ejercicio_31.Producto;
+
 public class PruebaProducto {
+    public static void main(String[] args) {
+        Producto p1 = Producto.crearProductoBasico("Mouse Inalámbrico", 45000.0);
+        Producto p2 = Producto.crearProductoBasico("Pad Mouse", 15000.0);
+
+        p1.mostrarProducto();
+        p2.mostrarProducto();
+    }
 }

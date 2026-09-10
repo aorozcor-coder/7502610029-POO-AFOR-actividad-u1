41919b6276a4ebf4a5cffe461c65d0320cbe530c
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-09-10 11:23:39 -0500

    Ejercicio 30: Implementa pruebaEstudiante con el fin de instanciar cada parametro de la clase Estudiante

--- src/ParteF_VariasFormasConstruir/Ejercicio_30/PruebaEstudiante.java
+++ src/ParteF_VariasFormasConstruir/Ejercicio_30/PruebaEstudiante.java
@@ -1,4 +1,20 @@
 package ParteF_VariasFormasConstruir.Ejercicio_30;
 
+import ParteF_VariasFormasConstruir.Ejercicio_29.Estudiante;
+
 public class PruebaEstudiante {
+    public static void main(String[] args) {
+        // Uso del constructor vacío
+        Estudiante e1 = new Estudiante();
+
+        // Uso del constructor con nombre y código
+        Estudiante e2 = new Estudiante("Laura Perez", "1526");
+
+        // Uso del constructor con nombre, código y semestre
+        Estudiante e3 = new Estudiante("Mateo Rios", "5846", 4);
+
+        e1.mostrarInfo();
+        e2.mostrarInfo();
+        e3.mostrarInfo();
+    }
 }

333d1e388d63542d294c920c318b54ce04bb2d67
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-09-09 19:20:22 -0500

    Actualiza PruebaCuentaBancaria con el código completo del ejercicio 10

--- src/ParteB_Objetos/Ejercicio_10/PruebaCuentaBancaria.java
+++ src/ParteB_Objetos/Ejercicio_10/PruebaCuentaBancaria.java
@@ -1,4 +1,26 @@
 package ParteB_Objetos.Ejercicio_10;
 
+import ParteA_Clases.Ejercicio_04.CuentaBancaria;
+
 public class PruebaCuentaBancaria {
+    public static void main(String[] args) {
+        CuentaBancaria c1 = new CuentaBancaria();
+        c1.numero = "100-001";
+        c1.titular = "Ana Martínez";
+        c1.saldo = 500000.0;
+
+        CuentaBancaria c2 = new CuentaBancaria();
+        c2.numero = "100-002";
+        c2.titular = "Roberto Gómez";
+        c2.saldo = 1200000.0;
+
+        System.out.println("--- Cuenta 1 ---");
+        c1.mostrarCuenta();
+
+        System.out.println("\n--- Cuenta 2 ---");
+        c2.mostrarCuenta();
+    }
 }
+
+
+

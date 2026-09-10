8366d77e53b869f127843ec08f54bf2b4058732c
aorozcor-coder <aorozcor@unicartagena.edu.co>
2026-09-09 23:16:27 -0500

    Ejercicio 20: Actualiza codigo completo

--- src/ParteD_ClaseVsObjeto/Ejercicio_20/Celular.java
+++ src/ParteD_ClaseVsObjeto/Ejercicio_20/Celular.java
@@ -1,4 +1,13 @@
 package ParteD_ClaseVsObjeto.Ejercicio_20;
 
 public class Celular {
+    String marca;
+    String modelo;
+    int almacenamiento;
+
+    public Celular(String marca, String modelo, int almacenamiento){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.almacenamiento = almacenamiento;
+    }
 }
--- src/ParteD_ClaseVsObjeto/Ejercicio_20/PruebaTresObjetos.java
+++ src/ParteD_ClaseVsObjeto/Ejercicio_20/PruebaTresObjetos.java
@@ -1,4 +1,23 @@
 package ParteD_ClaseVsObjeto.Ejercicio_20;
 
 public class PruebaTresObjetos {
+    public static void main(String[] args) {
+        Celular c1 = new Celular("Apple", "IPhone 15", 128);
+        Celular c2 = new Celular("Samsung", "Galaxy S24", 256);
+        Celular c3 = new Celular("Xiaomi", "Redmi Note 13", 256);
+        /*
+         * EXPLICACIÓN EJERCICIO 20:
+         *
+         * ¿QUÉ COMPARTEN?
+         * Comparten la misma estructura (los atributos marca, modelo y almacenamientoGB)
+         * definida dentro del molde/plantilla que es la clase Celular.
+         *
+         * ¿EN QUÉ SE DIFERENCIAN?
+         * Se diferencian en su identidad (cada objeto c1, c2 y c3 ocupa un lugar distinto en memoria RAM)
+         * y en su ESTADO, que son los valores específicos guardados en sus atributos (ej: marca, modelo y capacidad).
+         */
+        System.out.println("Celular 1: " + c1.marca + " " + c1.modelo + " (" + c1.almacenamiento + "GB)");
+        System.out.println("Celular 2: " + c2.marca + " " + c2.modelo + " (" + c2.almacenamiento + "GB)");
+        System.out.println("Celular 3: " + c3.marca + " " + c3.modelo + " (" + c3.almacenamiento + "GB)");
+    }
 }
